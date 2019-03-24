e278843693761ee3cb304e2367dc92fabf980664
Mauricio Renero Mendoza <mauriciorenero@gmail.com>
2019-03-24 17:02:03 -0600

    Finishing Third Activity

--- app/src/main/java/mau/train/com/appnoob/ThirdActivity.java
+++ app/src/main/java/mau/train/com/appnoob/ThirdActivity.java
@@ -1,11 +1,15 @@
 package mau.train.com.appnoob;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +20,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 public class ThirdActivity extends AppCompatActivity {
@@ -23,9 +28,10 @@ public class ThirdActivity extends AppCompatActivity {
     private ImageButton phoneBtn;
     private EditText mailTxt;
     private EditText phoneTxt;
-    private Button thirdBtn;
+    private ImageButton cameraBtn;
 
     private final int PHONE_CALL_CODE = 100;
+    private final int CAMERA_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +42,7 @@ public class ThirdActivity extends AppCompatActivity {
         phoneBtn = findViewById(R.id.phoneBtn);
         mailTxt = findViewById(R.id.mailTxt);
         phoneTxt = findViewById(R.id.phoneTxt);
-        thirdBtn = findViewById(R.id.thirdBtn);
+        cameraBtn = findViewById(R.id.cameraBtn);
 
         this.phoneBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,9 +51,27 @@ public class ThirdActivity extends AppCompatActivity {
                 if(phoneNumber != null && !TextUtils.isEmpty(phoneNumber)){
                     //comprobar version de android
                     if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                        Toast.makeText(ThirdActivity.this,"Version Mayor",Toast.LENGTH_SHORT).show();
-                        //esto es para las nuevas versiones. Dispara el metodo onRequestPermission
-                        requestPermissions(new String[]{Manifest.permission.CALL_PHONE},PHONE_CALL_CODE);
+                        //comprobar si el usuario ha aceptado, no ha aceptado o nunca se le ha preguntado
+                        //ha aceptado
+                        if(CheckPermission(Manifest.permission.CALL_PHONE)){
+                            Intent intent = new Intent(Intent.ACTION_CALL,Uri.parse("tel:" + phoneNumber));
+                            if(ActivityCompat.checkSelfPermission(ThirdActivity.this,Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) return;
+                            startActivity(intent);
+                        }else{
+                            //No se le ha preguntado
+                            if(!shouldShowRequestPermissionRationale(Manifest.permission.CALL_PHONE)){
+                                requestPermissions(new String[]{Manifest.permission.CALL_PHONE},PHONE_CALL_CODE);
+                            }else{ //ha denegado
+                                Toast.makeText(ThirdActivity.this,"Please, enable CALL_PHONE permission",Toast.LENGTH_SHORT).show();
+                                Intent intentSettings = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                intentSettings.addCategory(Intent.CATEGORY_DEFAULT);
+                                intentSettings.setData(Uri.parse("package:" + getPackageName()));
+                                intentSettings.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intentSettings.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                                intentSettings.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                                startActivity(intentSettings);
+                            }
+                        }
                     }else
                         olderVersions(phoneNumber);
                 }else
@@ -61,7 +85,62 @@ public class ThirdActivity extends AppCompatActivity {
                 }else{
                     Toast.makeText(ThirdActivity.this,"You declined the permission", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
 
+        this.mailBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //String url = mailTxt.getText().toString();
+                String mail = "mauriciorenero@gmail.com";
+                //if(url != null && !url.isEmpty()){
+                    //Intent intentWeb = new Intent(Intent.ACTION_VIEW,Uri.parse("http://"));
+                    //intentWeb.setAction(Intent.ACTION_VIEW);
+                    //intentWeb.setData(Uri.parse("http://" + url));
+
+                    //Contacts
+                    //Intent intentContact = new Intent(Intent.ACTION_VIEW,Uri.parse("content://contacts/people"));
+
+                    //Email Rapido
+                    //Intent intentMail = new Intent(Intent.ACTION_SENDTO,Uri.parse("mailto:"+url));
+
+                    //Email Completo
+                    Intent intentMailC = new Intent(Intent.ACTION_SEND,Uri.parse(mail));
+                    //intentMailC.setClassName("com.google.android.gm","com.google.android.gm.ComposeActivityGmail");
+                    intentMailC.setType("plain/text");
+                    intentMailC.putExtra(Intent.EXTRA_SUBJECT,"Title");
+                    intentMailC.putExtra(Intent.EXTRA_TEXT,"Hello World");
+                    intentMailC.putExtra(Intent.EXTRA_EMAIL,new String[]{"mauriciorenerode3@gmail.com"});
+
+                    PackageManager packageManager = getPackageManager();
+                    List activities = packageManager.queryIntentActivities(intentMailC,PackageManager.MATCH_DEFAULT_ONLY);
+                    if(activities.size() > 0){
+                        Intent intentChooser = Intent.createChooser(intentMailC,"Selecciona un cliente");
+                        if(intentMailC.resolveActivity(getPackageManager())!= null){
+                            Toast.makeText(ThirdActivity.this,"Entró",Toast.LENGTH_SHORT).show();
+                            startActivity(intentChooser);
+                        }else
+                            Toast.makeText(ThirdActivity.this,"No se puede abrir nada",Toast.LENGTH_SHORT).show();
+                    }else
+                        Toast.makeText(ThirdActivity.this,"No hay app",Toast.LENGTH_SHORT).show();
+
+
+
+                //}else
+                //    Toast.makeText(ThirdActivity.this,"Por favor, llene el campo",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        this.cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    requestPermissions(new String[]{Manifest.permission.CAMERA},CAMERA_CODE);
+                    startActivityForResult(intentCamera,CAMERA_CODE);
+                }else {
+                    Toast.makeText(ThirdActivity.this,"No mame actualicese",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -88,8 +167,42 @@ public class ThirdActivity extends AppCompatActivity {
                     }
                 }
                 break;
+            case CAMERA_CODE:
+                String permissionCam = permissions[0];
+                int resultCam = grantResults[0];
+                if(permissionCam.equals(Manifest.permission.CAMERA)){
+                    if(resultCam == PackageManager.PERMISSION_GRANTED){
+                        //Concedio los permisos
+                        Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                            return;
+                        }
+                        startActivity(intentCamera);
+                    }else{
+                        //No concedio su permiso
+                        Toast.makeText(ThirdActivity.this,"You declined the permission",Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
             default:
                 super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode){
+            case CAMERA_CODE:
+                if(resultCode == Activity.RESULT_OK){
+                    String result = data.toUri(0);
+                    Toast.makeText(this,"Result: " + result,Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(this,"There was an error",Toast.LENGTH_LONG).show();
+                }
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
                 break;
         }
     }
